e4f35c68f50909eb676284ba208a83b6a6e92306
xneyder <xneyder@gmail.com>
2023-07-26 19:29:32 -0400

    Add files

--- variables.tf
+++ variables.tf
@@ -251,7 +251,7 @@ variable "user_group_ids" {
 }
 
 variable "auto_minor_version_upgrade" {
-  type        = bool
+  type        = string
   default     = null
   description = "Specifies whether minor version engine upgrades will be applied automatically to the underlying Cache Cluster instances during the maintenance window. Only supported if the engine version is 6 or higher."
 }
