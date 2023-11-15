8e1f7deaa4ae4d5726d2faffc79bfd41302357f6
xneyder <xneyder@gmail.com>
2023-11-15 15:23:33 -0500

    fix

--- main.tf
+++ main.tf
@@ -38,11 +38,6 @@ locals {
 #   tags        = var.security_group_tags
 # }
 
-data "aws_security_group" "default" {
-  for_each = module.this.enabled ? toset(var.security_groups) : []
-
-  name = each.key
-}
 
 # resource "aws_security_group_rule" "default" {
 #   for_each = local.create_security_group ? var.security_group_rules : {}
@@ -109,6 +104,16 @@ resource "aws_elasticache_parameter_group" "default" {
 #   name  = var.security_group_name
 # }
 
+# data "aws_security_group" "default" {
+#   for_each = module.this.enabled ? toset(var.security_groups) : []
+
+#   name = each.key
+# }
+
+data "aws_security_group" "default" {
+  name = "dev-redis-allow-ec2"
+}
+
 resource "aws_elasticache_replication_group" "default" {
   count = module.this.enabled ? 1 : 0
 
@@ -126,7 +131,7 @@ resource "aws_elasticache_replication_group" "default" {
   # It would be nice to remove null or duplicate security group IDs, if there are any, using `compact`,
   # but that causes problems, and having duplicates does not seem to cause problems.
   # See https://github.com/hashicorp/terraform/issues/29799
-  security_group_ids         = [join("", data.aws_security_group.default[*].id)]
+  security_group_ids         = [join("", data.aws_security_group.default[*].arn)]
   security_group_names       = var.security_group_names
   maintenance_window         = var.maintenance_window
   notification_topic_arn     = var.notification_topic_arn
