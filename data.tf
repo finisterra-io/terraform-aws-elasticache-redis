f93618a310f00eff4b9091a272865e0fd1f0809a
xneyder <xneyder@gmail.com>
2023-11-15 14:30:59 -0500

    fix

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aws_vpc" "default" {
 
 data "aws_subnet" "default" {
   count  = module.this.enabled && var.subnet_names != null ? length(var.subnet_names) : 0
-  vpc_id = data.aws_vpc.default[0].id
+  vpc_id = var.vpc_name != null ? data.aws_vpc.default[0].id : var.vpc_id
   filter {
     name   = "tag:Name"
     values = [var.subnet_names[count.index]]
