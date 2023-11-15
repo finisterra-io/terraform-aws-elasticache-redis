1f4c04d103d54b25209afc2cae50d9a1b525e055
xneyder <xneyder@gmail.com>
2023-11-15 14:57:22 -0500

    fix

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ locals {
 # }
 
 data "aws_security_group" "default" {
-  for_each = module.this.enabled ? toset(var.security_groups) : {}
+  for_each = module.this.enabled ? var.security_groups : []
 
   name = each.value
 }
